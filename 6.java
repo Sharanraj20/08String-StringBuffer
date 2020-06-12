f6c1d74c99a3ba287f0698020a14c1eb0b4d825f
Sharan Raj G <60322880+Sharanraj20@users.noreply.github.com>
2020-06-12 17:25:14 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Triple
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		int n = a.length();
+		int m = b.length();
+		String result = "";
+		if(n<m)
+		{
+			result = a+b+a;
+		}
+		else
+		{
+			result = b+a+b;
+		}
+		System.out.println(result);
+		sc.close();
+	}
+}
+© 2020 GitHub, Inc.
